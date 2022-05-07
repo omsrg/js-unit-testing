e5a2846d30713d1369919f5d2ef2fea9e5b5465b
omsrg <siregar.om@gmail.com>
2022-05-07 16:19:58 +0700

    custom mocking logic

--- mocking-spies/src/util/io.test.js
+++ mocking-spies/src/util/io.test.js
@@ -4,6 +4,15 @@ import { promises as fs } from 'fs';
 import writeData from './io';
 
 vi.mock('fs');
+vi.mock('path', () => {
+	return {
+		default: {
+			join: (...args) => {
+				return args[args.length - 1];
+			},
+		},
+	};
+});
 
 it('should execute the writeFile method', () => {
 	const testData = 'Test';
@@ -12,5 +21,6 @@ it('should execute the writeFile method', () => {
 	writeData(testData, testFileName);
 
 	// return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
-	expect(fs.writeFile).toBeCalled();
+	// expect(fs.writeFile).toBeCalled();
+	expect(fs.writeFile).toBeCalledWith(testFileName, testData);
 });
