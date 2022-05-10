8c88f5ab509c53dfb9ea7d8ce0ba829b32837815
omsrg <siregar.om@gmail.com>
2022-05-11 00:40:13 +0700

    added resources

---
+++ dom/util/dom.test.js
@@ -0,0 +1,46 @@
+import { beforeEach, expect, it, vi } from 'vitest';
+import { Window } from 'happy-dom';
+
+import fs from 'fs';
+import path from 'path';
+import { showError } from './dom';
+
+const htmlDocPath = path.join(process.cwd(), 'index.html');
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
+
+const window = new Window();
+const document = window.document;
+document.write(htmlDocumentContent);
+vi.stubGlobal('document', document);
+
+beforeEach(() => {
+	document.body.innerHTML = '';
+	document.write(htmlDocumentContent);
+});
+
+it('should add an error paragraph to the id="errors" element', () => {
+	showError('test');
+
+	const errorsEl = document.getElementById('errors');
+	const errorParagraph = errorsEl.firstElementChild;
+
+	expect(errorParagraph).not.toBeNull();
+});
+
+it('should not contain an error paragraph initially', () => {
+	const errorsEl = document.getElementById('errors');
+	const errorParagraph = errorsEl.firstElementChild;
+
+	expect(errorParagraph).toBeNull();
+});
+
+it('should output the provided message in the error paragraph', () => {
+	const testErrorMessage = 'Test message';
+
+	showError(testErrorMessage);
+
+	const errorsEl = document.getElementById('errors');
+	const errorParagraph = errorsEl.firstElementChild;
+
+	expect(errorParagraph.textContent).toBe(testErrorMessage);
+});
